775fec239637dc73cb6e0cfd43e9d1b13e8c9035
BrenMurrell <bren.murrell@nvinteractive.com>
2015-11-12 20:11:50 +1300

    adding functions.php and including image size code

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 400, 99999 ); // Normal post thumbnails (see options below)
+add_image_size( 'sz-large', 800, 9999 ); // Permalink thumbnail size
+add_image_size( 'sz-full', 1200, 9999 ); // Permalink thumbnail size
